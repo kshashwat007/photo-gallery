90187b14854b455a13f1ea687e13ff6c7eb9c2b5
kshashwat007 <kshashwat007@gmail.com>
2021-05-02 13:25:24 +0400

    test change

--- App.js
+++ App.js
@@ -52,7 +52,7 @@ export default function App() {
   return (
     <NavigationContainer theme={MyTheme}>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Let's do this 🔥🚀</Text>
+        <Text>Let's do thisss 🔥🚀</Text>
       </View>
     </NavigationContainer>
   );
