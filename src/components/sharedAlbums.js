915c19ab108c0f7571c9de2b322bb462a18902ab
kshashwat007 <kshashwat007@gmail.com>
2021-05-19 19:24:53 +0400

    avatar container

--- src/components/sharedAlbums.js
+++ src/components/sharedAlbums.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import theme from '../../assets/themes';
+import Avatars from './Avatars';
 import SharedAlbumCard from './SharedAlbumCard';
 
 const SharedAlbums = ({ route }) => {
@@ -7,8 +9,32 @@ const SharedAlbums = ({ route }) => {
   return (
     <ScrollView>
       <SharedAlbumCard album={album} />
+      <View style={styles.avatarContainer}>
+        <Avatars avatars={album.avatars} />
+        <Text
+          style={styles.numberStyle}
+        >{`${album.avatars.length} people`}</Text>
+      </View>
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  avatarContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: theme.colors.lightGray,
+    margin: theme.spacing.m,
+    marginVertical: theme.spacing.l,
+    paddingHorizontal: theme.spacing.m,
+    paddingVertical: theme.spacing.xs,
+    borderRadius: theme.borderRadius.m
+  },
+  numberStyle: {
+    ...theme.textVariants.body3,
+    color: theme.colors.gray
+  }
+});
 
 export default SharedAlbums;
