250f796de1100474ae8706103cd035a97bdc8dd2
kshashwat007 <kshashwat007@gmail.com>
2021-05-19 13:39:26 +0400

    shared album card styling

--- src/components/SharedAlbumCard.js
+++ src/components/SharedAlbumCard.js
@@ -4,10 +4,17 @@ import theme from '../../assets/themes/index';
 
 const SharedAlbumCard = ({ album }) => {
   return (
-    <ImageBackground
-      source={album.background}
-      style={styles.imageBackground}
-    ></ImageBackground>
+    <ImageBackground source={album.background} style={styles.imageBackground}>
+      <View style={styles.imageContentContainer}>
+        <View>
+          <Text style={styles.imageTitle}>{album.title}</Text>
+          <Text style={styles.imageSubtitle}>{`Created by ${album.user}`}</Text>
+        </View>
+        <View>
+          <Text style={styles.imageNumber}>102 Photos</Text>
+        </View>
+      </View>
+    </ImageBackground>
   );
 };
 
@@ -15,11 +22,30 @@ const styles = StyleSheet.create({
   imageBackground: {
     resizeMode: 'cover',
     overflow: 'hidden',
-    height: theme.imageHeight.m,
-    margin: theme.spacing.m,
+    height: theme.imageHeight.l,
+    marginTop: theme.spacing.m,
+    marginHorizontal: theme.spacing.m,
     paddingHorizontal: theme.spacing.m,
+    paddingVertical: theme.spacing.m,
     borderRadius: theme.borderRadius.m,
-    justifyContent: 'center'
+    justifyContent: 'flex-end'
+  },
+  imageContentContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between'
+  },
+  imageTitle: {
+    ...theme.textVariants.h1,
+    color: theme.colors.white
+  },
+  imageSubtitle: {
+    ...theme.textVariants.body2,
+    color: theme.colors.white
+  },
+  imageNumber: {
+    ...theme.textVariants.body3,
+    color: theme.colors.white
   }
 });
 
